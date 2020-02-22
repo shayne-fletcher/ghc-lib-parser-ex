32689a0a905eef6bf8491c75ebfb36645b17da31
Shayne Fletcher <shayne@shaynefletcher.org>
2020-02-22 13:43:13 -0500

    Skeleton for Pat.hs

---
+++ src/Language/Haskell/GhclibParserEx/GHC/Hs/Pat.hs
@@ -0,0 +1,18 @@
+-- Copyright (c) 2020, Shayne Fletcher. All rights reserved.
+-- SPDX-License-Identifier: BSD-3-Clause.
+
+{-# OPTIONS_GHC -Wno-missing-fields #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE LambdaCase #-}
+#include "ghclib_api.h"
+module Language.Haskell.GhclibParserEx.GHC.Hs.Pat(
+  ) where
+
+{-
+#if defined (GHCLIB_API_811) || defined (GHCLIB_API_810)
+import GHC.Hs
+#else
+import HsSyn
+#endif
+import SrcLoc
+-}

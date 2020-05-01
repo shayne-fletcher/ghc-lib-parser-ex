7656cdf990ea252366018255f4909e04d7178421
Shayne Fletcher <shayne@shaynefletcher.org>
2020-05-01 16:33:05 -0400

    Move Parser and Outputable modules

---
+++ src/Language/Haskell/GhclibParserEx/GHC/Utils/Outputable.hs
@@ -0,0 +1,18 @@
+-- Copyright (c) 2020, Shayne Fletcher. All rights reserved.
+-- SPDX-License-Identifier: BSD-3-Clause.
+
+{-# LANGUAGE CPP #-}
+#include "ghclib_api.h"
+module Language.Haskell.GhclibParserEx.GHC.Utils.Outputable (
+    unsafePrettyPrint
+)
+where
+
+#if defined (GHCLIB_API_811)
+import GHC.Utils.Outputable
+#else
+import Outputable
+#endif
+
+unsafePrettyPrint :: Outputable a => a -> String
+unsafePrettyPrint = showSDocUnsafe . ppr

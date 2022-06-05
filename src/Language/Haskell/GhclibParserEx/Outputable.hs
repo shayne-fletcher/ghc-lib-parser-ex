39eb6478db5d084661d75287ca7f66e1a6415e13
Shayne Fletcher <shayne@shaynefletcher.org>
2022-06-05 15:49:49 +1000

    remove deprecated modules

--- src/Language/Haskell/GhclibParserEx/Outputable.hs
+++
@@ -1,9 +0,0 @@
--- Copyright (c) 2020, Shayne Fletcher. All rights reserved.
--- SPDX-License-Identifier: BSD-3-Clause.
-
-module Language.Haskell.GhclibParserEx.Outputable
-  {-# DEPRECATED "Use Language.Haskell.GhclibParserEx.GHC.Utils.Outputable instead" #-} (
-  module Language.Haskell.GhclibParserEx.GHC.Utils.Outputable
-  )
-  where
-import Language.Haskell.GhclibParserEx.GHC.Utils.Outputable
--- src/Language/Haskell/GhclibParserEx/Parse.hs
+++
@@ -1,9 +0,0 @@
--- Copyright (c) 2020, Shayne Fletcher. All rights reserved.
--- SPDX-License-Identifier: BSD-3-Clause.
-
-module Language.Haskell.GhclibParserEx.Parse
-  {-# DEPRECATED "Use Language.Haskell.GhclibParserEx.GHC.Parser instead" #-} (
-  module Language.Haskell.GhclibParserEx.GHC.Parser
-  )
-  where
-import Language.Haskell.GhclibParserEx.GHC.Parser

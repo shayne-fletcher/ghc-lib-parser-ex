5b42208cae9ff1b71f91a1c8993814912a4308ac
Shayne Fletcher <shayne@shaynefletcher.org>
2023-07-04 19:38:54 -0400

    package upgrades (#154)

--- src/Language/Haskell/GhclibParserEx/GHC/Driver/Flags.hs
+++ src/Language/Haskell/GhclibParserEx/GHC/Driver/Flags.hs
@@ -6,13 +6,13 @@
 #include "ghclib_api.h"
 module Language.Haskell.GhclibParserEx.GHC.Driver.Flags () where
 
-#if !(defined (GHCLIB_API_HEAD) && !defined (GHCLIB_API_908) || defined(GHCLIB_API_906) || defined(GHCLIB_API_904)  || defined(GHCLIB_API_902) || defined (GHCLIB_API_900))
+#if ! (defined (GHCLIB_API_HEAD) || defined (GHCLIB_API_908) || defined(GHCLIB_API_906) || defined(GHCLIB_API_904)  || defined(GHCLIB_API_902) || defined (GHCLIB_API_900))
 import DynFlags
 #endif
 
 -- This instance landed in
 -- https://gitlab.haskell.org/ghc/ghc/merge_requests/2905.
-#if defined(GHCLIB_API_808) || defined(GHCLIB_API_810)
+#if defined (GHCLIB_API_808) || defined(GHCLIB_API_810)
 instance Bounded Language where
   minBound = Haskell98
   maxBound = Haskell2010

5ae8e722e0ac20aeca35c213d9fd5c4abf76e302
Shayne Fletcher <shayne@shaynefletcher.org>
2023-08-27 22:08:49 -0400

    test builds of old versions

---
+++ ghc-lib-parser-ex-runhaskell.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+
+stack runhaskell --stack-yaml stack-exact.yaml --package extra --package optparse-applicative CI.hs -- \
+      --version-tag 9.6.0.2
+
+stack runhaskell --stack-yaml stack.yaml --package extra --package optparse-applicative CI.hs -- \
+      --version-tag 9.6.0.2
+
+stack_yamls=( \
+  stack-961-927.yaml  \
+  stack-944-902.yaml  \
+  stack-926-902.yaml  \
+  stack-902-8107.yaml \
+  stack-810-808.yaml  \
+ )
+
+for stack_yaml in "${stack_yamls[@]}"; do
+  flavor="$(echo "$stack_yaml" | sed -r 's/^stack-([0-9]+)-(.*)\.yaml$/\1/')"
+  stack runhaskell --stack-yaml "$stack_yaml" --package extra --package optparse-applicative CI.hs -- --version-tag "$flavor"
+done

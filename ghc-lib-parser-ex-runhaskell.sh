9977168db407d952cd05a4f4c9520959f8f5e400
Shayne Fletcher <shayne@shaynefletcher.org>
2024-01-09 07:26:29 -0500

    ghc-9.6.4

--- ghc-lib-parser-ex-runhaskell.sh
+++ ghc-lib-parser-ex-runhaskell.sh
@@ -10,7 +10,7 @@ declare -A builds=( \
    [stack-926-902.yaml]=9.2.0.0
    [stack-944-902.yaml]=9.4.0.0
    [stack-948-902.yaml]=9.4.0.0
-   [stack-962-927.yaml]=9.6.0.0
+   [stack-964-927.yaml]=9.6.0.0
    [stack.yaml]=9.6.0.0
    [stack-exact.yaml]=9.8.0.0
 )

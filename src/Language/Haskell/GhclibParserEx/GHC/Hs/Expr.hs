b1278c1e15c804861b32377b975e317da189e82b
Shayne Fletcher <shayne@shaynefletcher.org>
2024-02-06 22:53:44 -0500

    updates for compat with ghc HEAD (#169)

--- src/Language/Haskell/GhclibParserEx/GHC/Hs/Expr.hs
+++ src/Language/Haskell/GhclibParserEx/GHC/Hs/Expr.hs
@@ -137,11 +137,13 @@ isQuasiQuoteSplice :: HsSplice GhcPs -> Bool
 #endif
 isQuasiQuoteSplice = \case HsQuasiQuote{} -> True; _ -> False
 
-
-#if ! ( defined (GHC_8_10) || defined (GHC_8_8) )
+#if ( defined (GHC_8_10) || defined (GHC_8_8) )
 isStrictMatch :: HsMatchContext GhcPs -> Bool
-#else
+#elif ( defined (GHC_9_8) || defined (GHC_9_6) || defined (GHC_9_4) || defined (GHC_9_2) || defined (GHC_9_0) )
 isStrictMatch :: HsMatchContext RdrName -> Bool
+#else
+-- ghc > 9.8.1
+isStrictMatch :: HsMatchContext (GenLocated SrcSpanAnnN RdrName) -> Bool
 #endif
 isStrictMatch = \case FunRhs{mc_strictness=SrcStrict} -> True; _ -> False
 

b10c8ce2341b3d0c400a375e31d097aa4ae1b79e
Shayne Fletcher <shayne@shaynefletcher.org>
2025-11-07 21:23:18 -0500

    ghc-9.14 GHC.Parser.Header.getOptions signature change

--- src/Language/Haskell/GhclibParserEx/GHC/Driver/Session.hs
+++ src/Language/Haskell/GhclibParserEx/GHC/Driver/Session.hs
@@ -197,8 +197,10 @@ parsePragmasIntoDynFlags flags (enable, disable) file str =
   catchErrors $ do
 #if (defined (GHC_9_2) || defined (GHC_9_0) || defined (GHC_8_10) || defined (GHC_8_8))
     let opts = getOptions flags (stringToStringBuffer str) file
-#elif (defined (GHC_9_14) || defined (GHC_9_12) || defined (GHC_9_10) || defined (GHC_9_8) || defined (GHC_9_6) || defined (GHC_9_4))
-    let (_, opts) = getOptions (initParserOpts flags) (stringToStringBuffer str) file
+#elif (defined (GHC_9_12) || defined (GHC_9_10) || defined (GHC_9_8) || defined (GHC_9_6) || defined (GHC_9_4))
+    let (_, opts) = getOptions (initParserOpts flags) (stringToStringBuffer str)
+#elif (defined (GHC_9_14))
+    let (_, opts) = getOptions (initParserOpts flags) (supportedLanguagePragmas flags) (stringToStringBuffer str) file
 #else
     let (_, opts) = getOptions (initParserOpts flags) (initSourceErrorContext flags) (supportedLanguagePragmas flags) (stringToStringBuffer str) file
 #endif
